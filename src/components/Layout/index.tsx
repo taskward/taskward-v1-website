f2b56964f3c8f7e51250def697e9d678de73c5f4
Bruce Song <recall4056@gmail.com>
2022-08-03 23:15:56 +0800

    Configure tailwindcss.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,11 +3,11 @@ import { Header, Sidebar } from "../index";
 
 export default function Layout(): JSX.Element {
   return (
-    <>
+    <div>
       123
       <Header />
       <Sidebar />
       <Outlet />
-    </>
+    </div>
   );
 }
