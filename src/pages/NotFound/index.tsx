3ac972a32c777722793d39bf214f479c86dfc832
Bruce Song <recall4056@gmail.com>
2022-08-07 17:15:51 +0800

    Add 404 page.

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,28 @@
 import { useTranslation } from "react-i18next";
+import taskward from "@assets/img/taskward.png";
+import { Link } from "react-router-dom";
 
 export default function NotFound(): JSX.Element {
   const { t } = useTranslation();
-  return <></>;
+  document.title = t("404.NOTFOUND") + " | Taskward";
+
+  return (
+    <div className="w-full h-full flex justify-center items-center select-none">
+      <div className="mb-24 flex justify-center items-center gap-8">
+        <div className="flex flex-col items-center gap-2">
+          <Link to="/">
+            <img
+              src={taskward}
+              loading="eager"
+              width="96"
+              height="96"
+              className="animate-bounce"
+            />
+          </Link>
+          <div className="text-md">Taskward</div>
+        </div>
+        <div className="text-5xl animate-pulse">404 {t("404.NOTFOUND")}</div>
+      </div>
+    </div>
+  );
 }
