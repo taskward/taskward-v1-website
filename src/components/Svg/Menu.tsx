91e00795a8ed9c9433654912608dd81a2ba1d733
Bruce Song <recall4056@gmail.com>
2022-08-08 15:35:24 +0800

    Add tooltip title.

--- src/components/Svg/Menu.tsx
+++ src/components/Svg/Menu.tsx
@@ -1,4 +1,5 @@
 import { type SvgProps } from ".";
+import { useTranslation } from "react-i18next";
 
 export default function Menu({
   width = "24",
@@ -6,16 +7,19 @@ export default function Menu({
   className = "fill-black",
   onClick,
 }: SvgProps): JSX.Element {
+  const { t } = useTranslation();
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width={width}
-      height={height}
-      className={className}
-      onClick={onClick}
-    >
-      <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"></path>
-    </svg>
+    <div title={t("HEADER.MENU")}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width={width}
+        height={height}
+        className={className}
+        onClick={onClick}
+      >
+        <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"></path>
+      </svg>
+    </div>
   );
 }
