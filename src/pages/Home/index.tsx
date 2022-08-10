79e363cf80e5a71e18dc177764f1cc331359bdc7
Bruce Song <recall4056@gmail.com>
2022-08-10 09:16:46 +0800

    Update.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,8 @@
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
 export default function Home(): JSX.Element {
-  return <>Home</>;
+  return (
+    <div className={clsx(styles.background, "w-screen h-screen")}>Home</div>
+  );
 }
