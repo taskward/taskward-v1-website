925c58bdc844ea209cca1da981af04bc9a61a5fc
Bruce Song <recall4056@gmail.com>
2022-08-11 00:45:52 +0800

    Temporary page.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,19 @@
 import clsx from "clsx";
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
 export default function Home(): JSX.Element {
   return (
-    <div className={clsx(styles.background, "w-screen h-screen")}>Home</div>
+    <div className={clsx(styles.background, "w-screen h-screen")}>
+      Home
+      <div className="absolute inset-x-1/2 inset-y-1/2 m-auto text-center">
+        <Link to="/note">
+          <h1 className="bg-white w-fit cursor-pointer rounded-md p-2 whitespace-nowrap hover:bg-slate-300">
+            Go to notes
+          </h1>
+        </Link>
+        <div className="whitespace-nowrap">This is a temporary page.</div>
+      </div>
+    </div>
   );
 }
