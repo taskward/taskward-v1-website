2e3bd34012c87ab1ff26abb16bef1d5a121009f9
Bruce Song <recall4056@gmail.com>
2022-08-12 22:19:39 +0800

    Update URL.

--- src/requests/axios.ts
+++ src/requests/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosService = axios.create({
-  baseURL: import.meta.env.VITE_BRUCE_WORLD_BASE_URL,
+  baseURL: import.meta.env.VITE_BRUCE_WORLD_API_BASE_URL,
   withCredentials: false,
 });
