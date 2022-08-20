26e775f4f9020ceb10e555d1f0ec0b31c39451c9
Bruce Song <recall4056@gmail.com>
2022-08-21 00:04:39 +0800

    Remove.

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,9 @@ import { store } from "@store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  //<React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  //</React.StrictMode>
 );
