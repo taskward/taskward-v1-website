4c417b3ed24a46412b8398bbd73057c53be2a446
Bruce Song <recall4056@gmail.com>
2022-08-26 09:05:24 +0800

    Update note UI.

--- src/pages/Note/index.tsx
+++ src/pages/Note/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 import { NoteCard } from "@components";
 import { getDocumentTitle } from "@utils";
@@ -24,8 +26,37 @@ export default function Note(): JSX.Element {
       description: "描述",
     },
     {
+      name: "TODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODOTODO",
+      description:
+        "描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述",
+    },
+    {
       name: "TODO",
       description: "描述",
+    },
+    {
+      name: "TODO",
+      description: "描述",
+    },
+    {
+      name: "TODO",
+      description: "描述",
+    },
+    {
+      name: "TODO",
+      description: "描述",
+    },
+    {
+      name: "TODO",
+      description: "描述",
+    },
+    {
+      name: "TODO",
+      description: "描述",
+    },
+    {
+      name: "TODO",
+      description: `1. What is state hoisting\n2. What are falsy values in js\n3. Synchronous vs Asynchronous code\n4. What is a purpose of a Function as a child\n5. Presentational component vs container component\n6. Why do we need keys for React lists\n7. How to recognize closures in js\n8. Difference between using fetch() and axios\n9. Purpose of fragments\n10. How to create a custom hook\n11. 闭包\n12. 继承\n13. 作用域和原型链\n14. this指向\n15. DOM操作\n16. 设计模式：工厂、单例、装饰器、发布订阅者`,
     },
   ]);
 
@@ -41,20 +72,31 @@ export default function Note(): JSX.Element {
 
   return (
     <div className="flex h-full flex-col">
-      <div className="mx-auto my-4 h-[100px] w-[600px] rounded-lg bg-white drop-shadow-lg"></div>
-      <div className="flex w-full flex-wrap overflow-y-scroll scroll-smooth bg-slate-200 p-2">
-        <div className="m-6 h-[300px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[300px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[400px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[500px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[700px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[300px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[300px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[400px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[300px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[300px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[500px] w-[200px] bg-slate-100">123</div>
-        <div className="m-6 h-[300px] w-[200px] bg-slate-100">123</div>
+      <div className="mx-auto my-4 h-[60px] w-[800px] shrink-0 rounded-lg bg-white drop-shadow-lg"></div>
+      <div
+        className={clsx(
+          "flex w-full flex-wrap overflow-y-scroll p-4",
+          styles.scrollbar
+        )}
+      >
+        <div className="mx-auto mb-80 w-[1000px]">
+          {noteList.map((note, index) => {
+            return (
+              <div
+                key={index}
+                className="mx-auto mb-4 flex h-fit w-[900px] flex-col gap-1 rounded-md bg-slate-100 p-4 dark:bg-slate-800"
+              >
+                <div className="block truncate text-lg font-medium">
+                  {note.name}
+                </div>
+                <p
+                  className="block whitespace-pre-line text-sm font-normal"
+                  dangerouslySetInnerHTML={{ __html: note.description }}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
