41ca2b29f614d4ffbda14045d0a2297c81102ba2
Bruce Song <recall4056@gmail.com>
2022-08-28 20:30:59 +0800

    Add React Query.

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,18 @@ import "./css/index.css";
 import "./i18n";
 import { store } from "@store";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   //<React.StrictMode>
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </Provider>
   //</React.StrictMode>
 );
