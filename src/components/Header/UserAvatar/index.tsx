ed50e4c58f0a94509df24e905834518f99e26d9c
Bruce Song <recall4056@gmail.com>
2022-08-29 22:29:53 +0800

    Temporary user default avatar.

--- src/components/Header/UserAvatar/index.tsx
+++ src/components/Header/UserAvatar/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { shallowEqual } from "react-redux";
 
+import { Icon } from "@components";
 import { userAction } from "@store";
 import { User } from "@interfaces";
 import { useAppSelector, useAppDispatch } from "@hooks";
@@ -21,13 +22,13 @@ export default function UserAvatar(): JSX.Element | null {
     navigate("/", { replace: true });
   }
 
-  if (user && user.avatarUrl) {
+  if (user) {
     return (
       <div
         className="flex cursor-pointer items-center justify-center rounded-full p-1 transition-colors hover:bg-gray-200 dark:hover:bg-gray-600"
-        onClick={logout}
+        // onClick={logout}
       >
-        {user.avatarUrl && (
+        {user.avatarUrl ? (
           <img
             src={user.avatarUrl}
             width="32"
@@ -35,6 +36,14 @@ export default function UserAvatar(): JSX.Element | null {
             loading="eager"
             className="pointer-events-none rounded-full"
           />
+        ) : (
+          <div className="flex h-8 w-8 items-center justify-center rounded-full bg-slate-50 dark:bg-slate-700">
+            <Icon.Face
+              width="28"
+              height="28"
+              className="fill-black dark:fill-white"
+            />
+          </div>
         )}
       </div>
     );
