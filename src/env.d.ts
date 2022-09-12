f1e8d11ff652344bbbe2f88406dc9e6d8aa1b394
Bruce <recall4056@gmail.com>
2022-09-12 14:08:32 +0800

    Remove env.

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_TASKWARD_BASE_URL: string;
   readonly VITE_BRUCE_WORLD_BASE_URL: string;
-  readonly VITE_BRUCE_WORLD_API_BASE_URL: string;
   readonly VITE_GITHUB_CLIENT_ID: string;
   readonly VITE_VERSION: string;
   readonly VITE_SHOW_REACT_QUERY_DEVTOOL: string;
