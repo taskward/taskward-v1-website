4d6d23e144cbab95c0b5a450fcbeed12daab31a0
Bruce Song <recall4056@gmail.com>
2022-10-01 08:57:20 +0800

    Update.

--- src/pages/Icons/index.tsx
+++ src/pages/Icons/index.tsx
@@ -2,54 +2,54 @@ import { Icon } from "@components";
 
 export default function Icons(): JSX.Element {
   return (
-    <div className="grid grid-flow-row grid-cols-8 gap-4 p-16">
-      <Icon.Add /> Add
-      <Icon.AddLink /> AddLink
-      <Icon.AddTask /> AddTask
-      <Icon.Archive /> Archive
-      <Icon.ArrowDropDown /> ArrowDropDown
-      <Icon.Cancel /> Cancel
-      <Icon.Check /> Check
-      <Icon.Close /> Close
-      <Icon.Copy /> Copy
-      <Icon.Delete /> Delete
-      <Icon.DeleteForever /> DeleteForever
-      <Icon.DeleteTime /> DeleteTime
-      <Icon.Door /> Door
-      <Icon.Edit /> Edit
-      <Icon.ExpandCircle /> ExpandCircle
-      <Icon.Face /> Face
-      <Icon.Info /> Info
-      <Icon.Key /> Key
-      <Icon.Label /> Label
-      <Icon.Language /> Language
-      <Icon.Link /> Link
-      <Icon.LinkOff /> LinkOff
+    <div className="grid h-screen grid-flow-row grid-cols-8 gap-4 p-16">
+      <Icon.Add className="dark:fill-white" /> Add
+      <Icon.AddLink className="dark:fill-white" /> AddLink
+      <Icon.AddTask className="dark:fill-white" /> AddTask
+      <Icon.Archive className="dark:fill-white" /> Archive
+      <Icon.ArrowDropDown className="dark:fill-white" /> ArrowDropDown
+      <Icon.Cancel className="dark:fill-white" /> Cancel
+      <Icon.Check className="dark:fill-white" /> Check
+      <Icon.Close className="dark:fill-white" /> Close
+      <Icon.Copy className="dark:fill-white" /> Copy
+      <Icon.Delete className="dark:fill-white" /> Delete
+      <Icon.DeleteForever className="dark:fill-white" /> DeleteForever
+      <Icon.DeleteTime className="dark:fill-white" /> DeleteTime
+      <Icon.Door className="dark:fill-white" /> Door
+      <Icon.Edit className="dark:fill-white" /> Edit
+      <Icon.ExpandCircle className="dark:fill-white" /> ExpandCircle
+      <Icon.Face className="dark:fill-white" /> Face
+      <Icon.Info className="dark:fill-white" /> Info
+      <Icon.Key className="dark:fill-white" /> Key
+      <Icon.Label className="dark:fill-white" /> Label
+      <Icon.Language className="dark:fill-white" /> Language
+      <Icon.Link className="dark:fill-white" /> Link
+      <Icon.LinkOff className="dark:fill-white" /> LinkOff
       <Icon.Loading /> Loading
-      <Icon.Lock /> Lock
-      <Icon.Login /> Login
-      <Icon.Logout /> Logout
-      <Icon.Menu /> Menu
-      <Icon.Moon /> Moon
-      <Icon.More /> More
-      <Icon.Note /> Note
-      <Icon.OpenLink /> OpenLink
-      <Icon.Pin /> Pin
-      <Icon.Refresh /> Refresh
-      <Icon.Remove /> Remove
-      <Icon.RestoreFromTrash /> RestoreFromTrash
-      <Icon.Search /> Search
-      <Icon.Settings /> Settings
-      <Icon.Sun /> Sun
+      <Icon.Lock className="dark:fill-white" /> Lock
+      <Icon.Login className="dark:fill-white" /> Login
+      <Icon.Logout className="dark:fill-white" /> Logout
+      <Icon.Menu className="dark:fill-white" /> Menu
+      <Icon.Moon className="dark:fill-white" /> Moon
+      <Icon.More className="dark:fill-white" /> More
+      <Icon.Note className="dark:fill-white" /> Note
+      <Icon.OpenLink className="dark:fill-white" /> OpenLink
+      <Icon.Pin className="dark:fill-white" /> Pin
+      <Icon.Refresh className="dark:fill-white" /> Refresh
+      <Icon.Remove className="dark:fill-white" /> Remove
+      <Icon.RestoreFromTrash className="dark:fill-white" /> RestoreFromTrash
+      <Icon.Search className="dark:fill-white" /> Search
+      <Icon.Settings className="dark:fill-white" /> Settings
+      <Icon.Sun className="dark:fill-white" /> Sun
       <Icon.Sync /> Sync
-      <Icon.Task /> Task
-      <Icon.Touch /> Touch
-      <Icon.Trash /> Trash
-      <Icon.Unarchive /> Unarchive
-      <Icon.Unlock /> Unlock
-      <Icon.Update /> Update
-      <Icon.Visibility /> Visibility
-      <Icon.VisibilityOff /> VisibilityOff
+      <Icon.Task className="dark:fill-white" /> Task
+      <Icon.Touch className="dark:fill-white" /> Touch
+      <Icon.Trash className="dark:fill-white" /> Trash
+      <Icon.Unarchive className="dark:fill-white" /> Unarchive
+      <Icon.Unlock className="dark:fill-white" /> Unlock
+      <Icon.Update className="dark:fill-white" /> Update
+      <Icon.Visibility className="dark:fill-white" /> Visibility
+      <Icon.VisibilityOff className="dark:fill-white" /> VisibilityOff
     </div>
   );
 }
